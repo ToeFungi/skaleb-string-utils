431526d6ad535fa7591c4c9f776df78d5dcc9add
Alex Pickering <pickeringacw@gmail.com>
2019-11-23 20:28:20 +0200

    Fix package.json and update index

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { toBinary } from './ToBinary'
 import { camelCase } from './CamelCase'
 import { capitalCase } from './CapitalCase'
 import { toHexadecimal } from './ToHexadecimal'
+import { Delimination } from './enums/Delimination'
 
-export { reverse, toBinary, camelCase, capitalCase, toHexadecimal }
+export { reverse, toBinary, camelCase, capitalCase, toHexadecimal, Delimination }
